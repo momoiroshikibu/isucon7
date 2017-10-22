4ede3258c06d45b65a4506a74c3d046170468455
mochi8k <y.hkh.185@gmail.com>
2017-10-22 14:35:40 +0900

    update restart.sh

--- recipes/restart.sh
+++ recipes/restart.sh
@@ -4,7 +4,7 @@ set -e
 
 now=`date +%Y%m%d-%H%M%S`
 
-mv /var/log/nginx/access.log /var/log/nginx/access.log.$now
+mv /var/log/nginx/access.log.ltsv /var/log/nginx/access.log.ltsv.$now
 systemctl reload nginx
 
 # mv /var/log/mysql/slow.log /var/log/mysql/slow.log.$now
